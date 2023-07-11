0b64d9f0cc80c5163712fe6b4d519bbeca19e68e
ViniSSantos <cepef.rh@gmail.com>
2023-07-10 23:29:52 -0300

    createStitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,31 @@
 //File-System Routing
 //Roteamento baseados em arquivos físicos.
+import { styled } from "../styles";
+
+const Button = styled('button', {
+  backgroundColor : '$rocketseat',
+  borderRadius : 4,
+  border : 0,
+  padding : '4px, 8px',
+
+
+  span: {
+   fontWeight : 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)',
+  },
+})
 
 export default function Home() {
   return (
     <>
-      <h1>Hello Word</h1>
+      <Button>
+        <span>777</span>  
+        Enviar
+      </Button> 
+      
     </>
   )
 }
